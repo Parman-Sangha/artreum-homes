2abd36a7438e89be482c44de8bd4a0bc35659b72
Kiru1288 <144362723+Kiru1288@users.noreply.github.com>
2024-11-29 14:35:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Home-Page': 'aartreum-homes' }); 
+    res.end("Welcome");
+});
+
+server.listen(3000, () => {
+    console.log("server running");
+});
