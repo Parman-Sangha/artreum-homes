dcde9ba174255ddf4122c657930a16b8c12c967e
Harsimran Preet Singh <153043163+04harsimran20@users.noreply.github.com>
2025-02-06 10:58:22 -0700

    Create AuthContext.js

---
+++ app/context/AuthContext.js
@@ -0,0 +1,29 @@
+"use client";
+import { createContext, useContext, useState, useEffect } from "react";
+import { auth } from "../firebase/firebaseConfig"; // Make sure this path is correct
+import { onAuthStateChanged, signOut } from "firebase/auth";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
