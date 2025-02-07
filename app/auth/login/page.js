17c829c4f54b476c96f7013aad8c5fd11bf757b9
Parman Sangha <105144305+Parman-Sangha@users.noreply.github.com>
2025-02-06 17:04:22 -0700

    idk atp

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -94,7 +94,7 @@ export default function LoginPage() {
 
           {/* Sign-Up Link */}
           <p className="mt-4">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <a href="/auth/signup" className="text-blue-500">
               Sign Up
             </a>
